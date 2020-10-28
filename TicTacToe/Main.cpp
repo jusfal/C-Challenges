2ef7c2272211eaed80d19bf2d01f24be20027188
jusfal <jfalcon@artfx.fr>
2020-10-28 18:36:33 +0100

    Updated update function and added win condition

--- TicTacToe/Main.cpp
+++ TicTacToe/Main.cpp
@@ -14,10 +14,9 @@ int turn = 1;
 void DrawBoard();
 void Instructions();
 void PlayerTurn();
-//void WinCondition(char* pBoardValue);
-char* UpdateBoard(char* boardValue, int* playerTurn);
+void WinCondition(array<array<char, 3>, 3>* pBoardValue);
+array<array<char, 3>, 3>* UpdateBoard(array<array<char, 3>, 3>* boardValue, int* playerTurn);
 
-//char board[3][3] = { {'0','1','2'},{'3', '4', '5'},{'6', '7', '8'} };
 array<array<char, 3>, 3> board = {{ {'0','1','2'},{'3', '4', '5'},{'6', '7', '8'} }};
 
 int main(int argc, char* args[])
@@ -69,32 +68,75 @@ void PlayerTurn()
 
 }
 
-char* UpdateBoard(char* pBoardValue, int* pPlayerChoice)
+array<array<char, 3>, 3>* UpdateBoard(array<array<char, 3>, 3>* pBoardValue, int* pPlayerChoice)
 {
+	//row
+	int x1 = *pPlayerChoice / 3;
+	//column
+	int x2 = *pPlayerChoice % 3;
 	if (turn == 1)
 	{
-		int x = *pPlayerChoice;
-		pBoardValue[x] = 'X';
-		turn = 2;
+		if (turn == 1 && (*pBoardValue)[x1][x2] == 'O')
+		{
+			cout << "Place is taken" << endl;
+		}
+		else
+		{
+			(*pBoardValue)[x1][x2] = 'X';
+			turn = 2;
+		}
 		
 	}
 	else if (turn == 2)
 	{
-		int x = *pPlayerChoice;
-		pBoardValue[x] = 'O';
-		turn = 1;
+		if (turn == 2 && (*pBoardValue)[x1][x2] == 'X')
+		{
+			cout << "Place is taken" << endl;
+		}
+		else
+		{
+			(*pBoardValue)[x1][x2] = 'O';
+			turn = 1;
+		}
+		
 	}
-
-
+	
 	return pBoardValue;
 }
 
 void WinCondition(array<array<char, 3>, 3>* pBoardValue)
 {
-	if (pBoardValue[0][0] == 'X' && pBoardValue[0][1] && pBoardValue[0][2])
+	//Row wins
+	int row1x = (*pBoardValue)[0][0] == 'X' && (*pBoardValue)[0][1] == 'X' && (*pBoardValue)[0][2] == 'X';
+	int row2x = (*pBoardValue)[1][0] == 'X' && (*pBoardValue)[1][1] == 'X' && (*pBoardValue)[1][2] == 'X';
+	int row3x = (*pBoardValue)[2][2] == 'X' && (*pBoardValue)[2][1] == 'X' && (*pBoardValue)[2][2] == 'X';
+	int row1o = (*pBoardValue)[0][0] == 'O' && (*pBoardValue)[0][1] == 'O' && (*pBoardValue)[0][2] == 'O';
+	int row2o = (*pBoardValue)[1][0] == 'O' && (*pBoardValue)[1][1] == 'O' && (*pBoardValue)[1][2] == 'O';
+	int row3o = (*pBoardValue)[2][2] == 'O' && (*pBoardValue)[2][1] == 'O' && (*pBoardValue)[2][2] == 'O';
+	//Column win
+	int col1x = (*pBoardValue)[0][0] == 'X' && (*pBoardValue)[1][0] == 'X' && (*pBoardValue)[2][0] == 'X';
+	int col2x = (*pBoardValue)[0][1] == 'X' && (*pBoardValue)[1][1] == 'X' && (*pBoardValue)[2][1] == 'X';
+	int col3x = (*pBoardValue)[0][2] == 'X' && (*pBoardValue)[1][2] == 'X' && (*pBoardValue)[2][2] == 'X';
+	int col1o = (*pBoardValue)[0][0] == 'O' && (*pBoardValue)[1][0] == 'O' && (*pBoardValue)[2][0] == 'O';
+	int col2o = (*pBoardValue)[0][1] == 'O' && (*pBoardValue)[1][1] == 'O' && (*pBoardValue)[2][1] == 'O';
+	int col3o = (*pBoardValue)[0][2] == 'O' && (*pBoardValue)[1][2] == 'O' && (*pBoardValue)[2][2] == 'O';
+	//Diangle win
+	int d1x = (*pBoardValue)[0][0] == 'X' && (*pBoardValue)[1][1] == 'X' && (*pBoardValue)[2][2] == 'X';
+	int d2x = (*pBoardValue)[0][2] == 'X' && (*pBoardValue)[1][1] == 'X' && (*pBoardValue)[2][0] == 'X';
+	int d1o = (*pBoardValue)[0][0] == 'O' && (*pBoardValue)[1][1] == 'O' && (*pBoardValue)[2][2] == 'O';
+	int d2o = (*pBoardValue)[0][2] == 'O' && (*pBoardValue)[1][1] == 'O' && (*pBoardValue)[2][0] == 'O';
+
+
+
+	if (row1x || row2x || row3x || col1x || col2x || col3x || d1x || d2x)
 	{
-		cout << "Winner" << endl;
+		cout << "Player 1 is the winner" << endl;
 	}
+	else if (row1o || row2o || row3o || col1o || col2o || col3o || d1o || d2o)
+	{
+		cout << "Player 2 is the winner" << endl;
+	}
+	
 }
 
 void Instructions()
@@ -103,4 +145,7 @@ void Instructions()
 	cout << "Welcome to TicTacToe! Enter any key to begin" << endl;
 	cin >> beginKey;
 }
+
+
+
 
